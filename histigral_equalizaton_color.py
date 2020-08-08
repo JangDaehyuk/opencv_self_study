36d241de60c50dd8c13f178c90bd147296cb5621
Jang DaeHyuk <49027524+JangDaehyuk@users.noreply.github.com>
2020-08-08 14:17:50 +0900

    Create histigral_equalizaton_color.py

---
+++ histigral_equalizaton_color.py
@@ -0,0 +1,21 @@
+# 컬러 영상의 히스토그램 평활화
+
+import numpy as np
+import cv2
+
+src = cv2.imread('sky.jpg')
+
+src_ycrcb = cv2.cvtColor(src, cv2.COLOR_BGR2YCrCb)
+ycrcb_planes = cv2.split(src_ycrcb)
+
+# 밝기 성분(Y)에 대해서만 히스토그램 평활화 수행
+ycrcb_planes[0] = cv2.equalizeHist(ycrcb_planes[0])
+
+dst_ycrcb = cv2.merge(ycrcb_planes)
+dst = cv2.cvtColor(dst_ycrcb, cv2.COLOR_YCrCb2BGR)
+
+cv2.imshow('src', src)
+cv2.imshow('dst', dst)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
